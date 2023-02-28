f43ecde6e9b78077110e76a2762020e097a0e149
Rachel Berg <rachelberg@Rachels-MacBook-Pro-5.local>
2023-02-28 10:56:47 -0600

    pca start

--- Rachel/rahcel script.R
+++ Rachel/rahcel script.R
@@ -1,1 +1,28 @@
-rachel
+library(tidyverse)
+library(lubridate)
+library(readxl)
+library(patchwork)
+library(janitor)
+## Load Anova Specific libraries -----
+library(car)
+library(emmeans)
+library(lme4)
+library(multcomp)
+library(multcompView)
+library(scales)  
+
+island.df <- read_csv("Resources DO NOT EDIT/rachel_data/mass_island_characteristics.csv")
+
+cor.test(island.df$weight_animal, island.df$island_distance, method = "pearson")
+
+#such a small sample size hard to describe sample to popualtion of island
+#use a PCA to describe
+install.packages("vegan")
+library(vegan)
+#factoextra
+
+test.df <- island.df %>% 
+  select(success, weight_animal, island_distance) %>% 
+  na.omit() 
+
+princomp(test.df)
